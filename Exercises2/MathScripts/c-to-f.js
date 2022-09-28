e3e9c0acdf47fa98fa2b3cf7daf797044719ed63
Ngoc <ngocbnguyen19@gmail.com>
2022-09-28 13:21:04 -0400

    ..

--- Exercises2/MathScripts/c-to-f.js
+++ Exercises2/MathScripts/c-to-f.js
@@ -3,4 +3,4 @@
 var fahrenheit = "75";
 var celsius = (fahrenheit - 32) * 5/9;
 
-console.log (Math.round(celsius));
+console.log (fahrenheit + "degree Fahrenheit is " + Math.round(celsius) + "degree Celsius");
