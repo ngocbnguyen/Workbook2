5256d2ff5414dc4552e5277060e82a5a937a4f5b
Ngoc <ngocbnguyen19@gmail.com>
2022-09-28 10:35:28 -0400

    Rule of 72

---
+++ Exercises2/MathScripts/rule-of-72.js
@@ -0,0 +1,4 @@
+var interestRate = "1";
+var timeToDouble = 72/interestRate;
+
+console.log ("At a " + interestRate + "% interest rate, your savings account will be doubled in " + timeToDouble.toFixed(2) + " years.");
