9f75bf000b18aadf2f19164e778af2e320621105
Ngoc <ngocbnguyen19@gmail.com>
2022-09-28 12:14:17 -0400

    temp degree conversion

---
+++ Exercises2/MathScripts/c-to-f.js
@@ -0,0 +1,6 @@
+"use strict"
+
+var fahrenheit = "75";
+var celsius = (fahrenheit - 32) * 5/9;
+
+console.log (Math.round(celsius));
---
+++ Exercises2/MathScripts/f-to-c.js
@@ -0,0 +1,6 @@
+"use strict";
+
+var celsius = "24";
+var fahrenheit = celsius * (9/5) + 32;
+
+console.log (Math.round(fahrenheit));
