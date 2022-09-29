7a6c97f10896d29ecd9acadc9a204c2ef8b033cd
Ngoc <ngocbnguyen19@gmail.com>
2022-09-29 10:03:25 -0400

    dept converter switch statement

---
+++ Exercise 8/dept-converter.js
@@ -0,0 +1,25 @@
+"use strict";
+let departmentCode = 20;
+switch (departmentCode) {
+    case 1:
+        console.log("Marketing");
+        break;
+    case 5:
+            console.log("Human Resouces");
+            break;
+    case 10:
+            console.log("Accounting");
+            break; 
+    case 12:
+            console.log("Legal");
+            break;
+    case 18:
+            console.log("IT");
+            break;
+    case 20:
+            console.log("Customer Relations");
+            break;                        
+    default:
+        console.log("Invalid Code");
+        break;
+}
---
+++ Exercises2/MathScripts/parse.js
@@ -0,0 +1,6 @@
+"use strict";
+
+var me = "33";
+console.log (typeof me);
+console.log (me + 2);
+
