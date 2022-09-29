b937eeff8729fa1012a642456cb01c82d55c03d3
Ngoc <ngocbnguyen19@gmail.com>
2022-09-29 10:15:54 -0400

    parse strings

---
+++ NumericConversions/conversion-tests.js
@@ -0,0 +1,41 @@
+"use strict";
+// Description: This script tests various numeric
+// conversion techniques
+// Author: Jordan Q. Newprogrammer
+
+let a = "  101.1  ", b = "55", c = "402 Stevens", d = "Number 5";
+// a = parseInt(a);
+// b = parseInt(b);
+// c = parseInt(c);
+// d = parseInt(d);
+// console.log(a);
+// console.log(b);
+// console.log(c);
+// console.log(d);
+
+// a = parseFloat(a);
+// b = parseFloat(b);
+// c = parseFloat(c);
+// d = parseFloat(d);
+// console.log(a);
+// console.log(b);
+// console.log(c);
+// console.log(d);
+
+// a = Number(a);
+// b = Number(b);
+// c = Number(c);
+// d = Number(d);
+// console.log(a);
+// console.log(b);
+// console.log(c);
+// console.log(d);
+
+a = +(a);
+b = +(b);
+c = +(c);
+d = +(d);
+console.log(a);
+console.log(b);
+console.log(c);
+console.log(d);
